3e271d32fe77f75fb4961d52ff6c7ba0e50798d4
jhpark1227 <dev.junhyeokpark@gmail.com>
2026-06-21 17:47:41 +0900

    전역 예외처리 로직 추가

--- src/main/java/junhyeok/blog/presentation/GlobalExceptionHandler.java
+++ src/main/java/junhyeok/blog/presentation/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,7 +18,9 @@ public class GlobalExceptionHandler {
 
     private final Map<ErrorCode, HttpStatus> errorCodeHttpStatusMapping = Map.of(
             ErrorCode.INVALID_CATEGORY_ID, HttpStatus.BAD_REQUEST,
-            ErrorCode.CATEGORY_NOT_FOUND, HttpStatus.NOT_FOUND
+            ErrorCode.CATEGORY_NOT_FOUND, HttpStatus.NOT_FOUND,
+            ErrorCode.POST_NOT_FOUND, HttpStatus.NOT_FOUND,
+            ErrorCode.NOTION_RESPONSE_INVALID, HttpStatus.INTERNAL_SERVER_ERROR
     );
 
     @ExceptionHandler(CustomException.class)
@@ -35,6 +38,13 @@ public class GlobalExceptionHandler {
                         MessageSourceResolvable::getDefaultMessage).toList());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse("INVALID_REQUEST"));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException exception) {
+        log.debug("[NO_RESOURCE_FOUND] {}", exception.getResourcePath());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("NOT_FOUND"));
     }
 
     @ExceptionHandler(Exception.class)
