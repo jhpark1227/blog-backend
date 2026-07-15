65e6f12e27d403da5fa8cf317a93dc6b94601d7e
jhpark1227 <dev.junhyeokpark@gmail.com>
2026-07-15 16:42:41 +0900

    카테고리 조회 시 게시글 카운트 로직 수정
    - 카테고리 기준 LEFT JOIN을 통해 게시글 카운트 로직 수정

--- src/main/java/junhyeok/blog/domain/CategoryRepository.java
+++ src/main/java/junhyeok/blog/domain/CategoryRepository.java
@@ -8,11 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
     @Query("""
-            SELECT new junhyeok.blog.application.dto.response.CategoryResponse(p.category.notionOptionId, p.category.name, COUNT(p.category))
-            FROM Post p
-            WHERE p.status = PostStatus.PUBLISHED
-            GROUP BY p.category
-            ORDER BY p.category.sortOrder ASC
+            SELECT new junhyeok.blog.application.dto.response.CategoryResponse(c.notionOptionId, c.name, COUNT(p))
+            FROM Category c
+            LEFT JOIN Post p ON p.category = c AND p.status = PostStatus.PUBLISHED
+            GROUP BY c
+            ORDER BY c.sortOrder ASC
             """)
     List<CategoryResponse> findAllWithCount();
 }
