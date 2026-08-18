bb768b61194ed553a58cec42986c44f46907b70c
jhpark1227 <dev.junhyeokpark@gmail.com>
2026-08-18 18:09:45 +0900

    카테고리 조회 인수 테스트 추가
    RestTestClient로 실제 HTTP 요청부터 DB 조회까지 검증한다.

---
+++ src/test/java/junhyeok/blog/acceptance/CategoryAcceptanceTest.java
@@ -0,0 +1,58 @@
+package junhyeok.blog.acceptance;
+
+import junhyeok.blog.domain.Category;
+import junhyeok.blog.domain.CategoryRepository;
+import junhyeok.blog.domain.PostRepository;
+import junhyeok.blog.domain.PostStatus;
+import junhyeok.blog.util.PostBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.test.web.servlet.client.RestTestClient.ResponseSpec;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
+class CategoryAcceptanceTest {
+
+    @Autowired
+    RestTestClient client;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @AfterEach
+    void tearDown() {
+        postRepository.deleteAllInBatch();
+        categoryRepository.deleteAllInBatch();
+    }
+
+    @Test
+    void 카테고리별_발행된_글_개수를_조회한다() {
+        Category category1 = categoryRepository.save(new Category("id1", "카테고리1", "red", 1));
+        Category category2 = categoryRepository.save(new Category("id2", "카테고리2", "blue", 2));
+        postRepository.save(PostBuilder.create(1).category(category1).build());
+        postRepository.save(PostBuilder.create(2).category(category1).build());
+        postRepository.save(PostBuilder.create(3).category(category2).status(PostStatus.PENDING).build());
+
+        ResponseSpec response = client.get()
+                .uri("/categories")
+                .exchange();
+
+        response.expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(2)
+                .jsonPath("$[0].id").isEqualTo("id1")
+                .jsonPath("$[0].name").isEqualTo("카테고리1")
+                .jsonPath("$[0].postCount").isEqualTo(2)
+                .jsonPath("$[1].id").isEqualTo("id2")
+                .jsonPath("$[1].name").isEqualTo("카테고리2")
+                .jsonPath("$[1].postCount").isEqualTo(0);
+    }
+}
