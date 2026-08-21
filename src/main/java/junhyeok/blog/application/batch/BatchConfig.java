b1b4147c13543f234dbcb7d94181bb51a29428e6
jhpark1227 <dev.junhyeokpark@gmail.com>
2026-08-21 15:30:42 +0900

    배치 메타데이터를 DB에 저장하도록 설정

--- src/main/java/junhyeok/blog/application/batch/BatchConfig.java
+++ src/main/java/junhyeok/blog/application/batch/BatchConfig.java
@@ -2,13 +2,14 @@ package junhyeok.blog.application.batch;
 
 import junhyeok.blog.domain.Post;
 import junhyeok.blog.domain.PostRepository;
+import org.springframework.batch.core.configuration.support.JdbcDefaultBatchConfiguration;
 import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.batch.infrastructure.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BatchConfig {
+public class BatchConfig extends JdbcDefaultBatchConfiguration {
 
     @Bean
     public ItemWriter<Post> postWriter(PostRepository postRepository) {
